0de252013aa87fd861ce280820d30b1d4862c76f
Troy Conrad Therrien <troyth@gmail.com>
2013-08-13 17:41:35 -0400

    removed .get

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,31 +7,6 @@ exports.index = function(req, res){
 };
 
 
-exports.get = function(req, res){
-	var url_parts = url.parse(req.url, true);
-	var url_array = url_parts.pathname.split('/');//gets rid of the preceding empty string
-
-	console.log('url_array: ');
-	console.dir(url_array);
-
-	switch(url_array[1]){
-		case 'v1':
-			switch(url_array[2]){
-				case 'machine':
-					if(url_array[3]){
-						case 
-					}
-
-					machine.getMachines(res);
-					break;
-			}
-			break;
-		default:
-			res.send(404, "please specify an API version number");
-			break;
-	}
-}
-
 exports.getMachine = function(req, res){
 	machine.getMachines(res);
 }
