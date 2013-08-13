7605320f31d5e258d480e4e8d03948e8d9ed5057
Troy Conrad Therrien <troyth@gmail.com>
2013-08-13 16:15:18 -0400

    testing routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,18 @@
 
-/*
- * GET home page.
- */
+var url = require('url');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+
+exports.get = function(req, res){
+	var url_parts = url.parse(req.url, true);
+	var url_array = url_parts.pathname.split('/');//gets rid of the preceding empty string
+
+	console.log('url_array: ');
+	console.dir(url_array);
+
+	console.log('url_parts: ');
+	console.dir(url_parts);
+}
